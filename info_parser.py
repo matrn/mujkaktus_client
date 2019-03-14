fee794bc11c21732428cc510a0c7400f280b5a8e
matrn <trnka.matej@seznam.cz>
2019-03-14 17:45:42 +0100

    updates

--- info_parser.py
+++ info_parser.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+t = '<div id="u80584eb3c1904eb2a234e74b1adf8beb"><p><span class="text-1">101,4 </span><span class="text-2">Kč</span></p><p>Platnost do 15.04.2019</p></div><div id="uae1db2a3fb5d4860bd78532a0a928f42"><div class="box-cell"><form method="post" action="https://www.mujkaktus.cz/moje-sluzby?p_p_auth=NR2gVwsO&p_p_id=kaktusvcc_WAR_vcc&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage" class="ajax"><fieldset><legend class="hide">Můj přehled</legend><input name="pageId" value="u3ca6f7ffb46544e98c67a24a59e546a4" type="hidden"/><div id="ud5501db414b9418fbc49f1be9ab95f95" class="box-padding"><div id="ud953bb06236c407587bcc24a4cb2fa70" class="equal-height"><p><span class="text-2">0:00</span><br/>minut</p><p></p></div><p><span class="ico ico-package-recurrent">samoobnovující</span></p><div class="action-zone"><a href="https://www.mujkaktus.cz/moje-sluzby?p_auth=8QOEf2zQ&p_p_auth=NR2gVwsO&p_p_id=kaktusvcc_WAR_vcc&p_p_lifecycle=1&p_p_state=normal&p_p_mode=view&_kaktusvcc_WAR_vcc_from=dashboard&_kaktusvcc_WAR_vcc_moduleCode=wholesalePrepaidFreeUnits" class="button">Koupit</a> </div></div></fieldset></form></div><div class="box-cell"><form method="post" action="https://www.mujkaktus.cz/moje-sluzby?p_p_auth=NR2gVwsO&p_p_id=kaktusvcc_WAR_vcc&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage" class="ajax"><fieldset><legend class="hide">Můj přehled</legend><input name="pageId" value="u3ca6f7ffb46544e98c67a24a59e546a4" type="hidden"/><div id="uf3e4f9bb5eb54b2aa88870f21760a9eb" class="box-padding"><div id="ue6e09bd3a25344f1a3436e7f983d6a39" class="equal-height"><p><span class="text-2">0</span><br/>SMS</p><p></p></div><p><span class="ico ico-package-recurrent">samoobnovující</span></p><div class="action-zone"><a href="https://www.mujkaktus.cz/moje-sluzby?p_auth=8QOEf2zQ&p_p_auth=NR2gVwsO&p_p_id=kaktusvcc_WAR_vcc&p_p_lifecycle=1&p_p_state=normal&p_p_mode=view&_kaktusvcc_WAR_vcc_from=dashboard&_kaktusvcc_WAR_vcc_moduleCode=wholesalePrepaidFreeUnits" class="button">Koupit</a> </div></div></fieldset></form></div><div class="box-cell"><form method="post" action="https://www.mujkaktus.cz/moje-sluzby?p_p_auth=NR2gVwsO&p_p_id=kaktusvcc_WAR_vcc&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage" class="ajax"><fieldset><legend class="hide">Můj přehled</legend><input name="pageId" value="u3ca6f7ffb46544e98c67a24a59e546a4" type="hidden"/><div id="u6cea944837ff414f826dd72a7f277bcd" class="box-padding"><div id="u1cd6af88ef0c4102905d8fbce4471956" class="equal-height"><div id="u1e011b2217904f2183beec2a99a81af2" class="clearfix"><div id="u5dedfaa1cb244628afe9df608b417559" class="col col-1-3"><p><span class="text-2">0</span><span class="block">MB</span><span class="text-small">Normální data</span></p></div><div id="ua0840f0e8da54fe7940470277ff37263" class="col col-1-3"><p><span class="text-2">+</span></p></div><div id="u5aceb72a7594b30ad2c33c3535c5b8f" class="col col-1-3"><p><span class="text-2">0</span><span class="block">MB</span><span class="text-small">Data na sociální sítě</span></p></div></div><p></p></div><p><span class="ico ico-package-recurrent">samoobnovující</span></p><div class="action-zone"><a href="https://www.mujkaktus.cz/moje-sluzby?p_auth=8QOEf2zQ&p_p_auth=NR2gVwsO&p_p_id=kaktusvcc_WAR_vcc&p_p_lifecycle=1&p_p_state=normal&p_p_mode=view&_kaktusvcc_WAR_vcc_from=dashboard&_kaktusvcc_WAR_vcc_moduleCode=wholesalePrepaidFreeUnits" class="button">Koupit</a> </div></div></fieldset></form></div></div>'
+t2 = '<div id="ubd76d17042f2413cb7469e2c04a88cf6"><p><span class="text-1">49 </span><span class="text-2">Kč</span></p><p>Platnost do 15.04.2019</p></div><div id="u7ac1a0ce5c254d828c3890bc22c24325"><div class="box-cell"><form method="post" action="https://www.mujkaktus.cz/moje-sluzby?p_p_auth=5yK2v1Ih&p_p_id=kaktusvcc_WAR_vcc&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage" class="ajax"><fieldset><legend class="hide">Můj přehled</legend><input name="pageId" value="u5c20be99349249f5b9bbf0f2b4638df4" type="hidden"/><div id="u2c4256d49f4d4ea1af6b508ac98f5288" class="box-padding"><div id="ub7d634ce884b403a8c75832d74f283d9" class="equal-height"><p><span class="text-2">0:00</span><br/>minut</p><p></p></div><p><span class="ico ico-package-recurrent">samoobnovující</span></p><div class="action-zone"><a href="https://www.mujkaktus.cz/moje-sluzby?p_auth=fhBGJ24C&p_p_auth=5yK2v1Ih&p_p_id=kaktusvcc_WAR_vcc&p_p_lifecycle=1&p_p_state=normal&p_p_mode=view&_kaktusvcc_WAR_vcc_from=dashboard&_kaktusvcc_WAR_vcc_moduleCode=wholesalePrepaidFreeUnits" class="button">Koupit</a> </div></div></fieldset></form></div><div class="box-cell"><form method="post" action="https://www.mujkaktus.cz/moje-sluzby?p_p_auth=5yK2v1Ih&p_p_id=kaktusvcc_WAR_vcc&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage" class="ajax"><fieldset><legend class="hide">Můj přehled</legend><input name="pageId" value="u5c20be99349249f5b9bbf0f2b4638df4" type="hidden"/><div id="u6f35aaa1c4734d7290bfed10192a61df" class="box-padding"><div id="u2c050d53f84d46a7adf64137cba71e1b" class="equal-height"><p><span class="text-2">0</span><br/>SMS</p><p></p></div><p><span class="ico ico-package-recurrent">samoobnovující</span></p><div class="action-zone"><a href="https://www.mujkaktus.cz/moje-sluzby?p_auth=fhBGJ24C&p_p_auth=5yK2v1Ih&p_p_id=kaktusvcc_WAR_vcc&p_p_lifecycle=1&p_p_state=normal&p_p_mode=view&_kaktusvcc_WAR_vcc_from=dashboard&_kaktusvcc_WAR_vcc_moduleCode=wholesalePrepaidFreeUnits" class="button">Koupit</a> </div></div></fieldset></form></div><div class="box-cell"><form method="post" action="https://www.mujkaktus.cz/moje-sluzby?p_p_auth=5yK2v1Ih&p_p_id=kaktusvcc_WAR_vcc&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_cacheability=cacheLevelPage" class="ajax"><fieldset><legend class="hide">Můj přehled</legend><input name="pageId" value="u5c20be99349249f5b9bbf0f2b4638df4" type="hidden"/><div id="uc371148f115e45ab8022df5cfe264a99" class="equal-height"><p><span class="text-1">73</span><br/>MB<br/>Platnost do<br/>20.02.2019</p></div><p><p><span class="ico ico-package-simple">jednorázový</span></p></p><div class="action-zone"><a href="https://www.mujkaktus.cz/moje-sluzby?p_auth=fhBGJ24C&p_p_auth=5yK2v1Ih&p_p_id=kaktusvcc_WAR_vcc&p_p_lifecycle=1&p_p_state=normal&p_p_mode=view&_kaktusvcc_WAR_vcc_from=dashboard&_kaktusvcc_WAR_vcc_moduleCode=wholesalePrepaidFreeUnits" class="button">Dokoupit</a> </div></fieldset></form></div></div>'
 
 from bs4 import BeautifulSoup
+'''
 
-soup = BeautifulSoup(t, 'html.parser')
 text_1 = soup.findAll("span", { "class" : "text-1" })
 text_2 = soup.findAll("span", { "class" : "text-2" })
 
@@ -10,8 +12,8 @@ credit = text_1[0]
 currency = text_2[0]
 
 span = soup.findAll("span")
-p = soup.findAll("p")
 
+'''
 
 credit = ""
 credit_unit = "Kč"
@@ -34,40 +36,66 @@ renew_keyword = "samoobnov"
 once_keyword = "jednor"
 
 #print(span)
-prev = ""
-for elm in p:
-	if len(elm.text) != 0:
-		print(elm)
+
+def parse_info(html):
+	'''
+	Types:
+
+		1 = jednorázový
+		2 = samoobnovující
+
+	return data - with examples:
+		'credit' : 101.5
+		'credit_expiration' : '11.11.2019'
+
+		'minutes' : 15.6
+		'minutes_type' : 1
 		
-		elm_s = str(elm)
-		#s = BeautifulSoup(elm, "html.parser")
+	'''
+	global credit, credit_unit, credit_expire, credit_expire_unit
+	global minutes, minutes_unit, minutes_type
+	global sms, sms_unit, sms_type
+	global mb_normal, mb_social, mb_type
+	global renew_keyword, once_keyword
 
-		if credit_unit.lower() in elm_s.lower():   #credit
-			credit = str(elm.find("span", { "class" : "text-1"}).text).lstrip()
-			prev = "credit"
-		
-		if credit_expire_unit.lower() in elm_s.lower() and len(credit) != 0:   #credit expiration
-			credit_expire = str(elm.text).replace(credit_expire_unit, "").lstrip()
-			prev = "credit"
+	soup = BeautifulSoup(html, 'html.parser')
+	p = soup.findAll("p")
 
+	prev = ""
+	for elm in p:
+		if len(elm.text) != 0:
+			print(elm)
+			
+			elm_s = str(elm)
+			#s = BeautifulSoup(elm, "html.parser")
 
-		if minutes_unit.lower() in elm_s.lower():   #minutes
-			minutes = str(elm.find("span", { "class" : "text-2"}).text).lstrip()
-			prev = "mins"
-
-
-		if sms_unit.lower() in elm_s.lower():   #minutes
-			sms = str(elm.find("span", { "class" : "text-2"}).text).lstrip()
-			prev = "sms"
-
-
-		if renew_keyword.lower() in elm_s.lower() or once_keyword.lower() in elm_s.lower():
-			if prev == "mins": minutes_type = renew_keyword
-			if prev == "sms": sms_type = renew_keyword
-
-			prev = "type"
+			if credit_unit.lower() in elm_s.lower():   #credit
+				credit = str(elm.find("span", { "class" : "text-1"}).text).lstrip()
+				prev = "credit"
+			
+			if credit_expire_unit.lower() in elm_s.lower() and len(credit) != 0:   #credit expiration
+				credit_expire = str(elm.text).replace(credit_expire_unit, "").lstrip()
+				prev = "credit"
 
 
+			if minutes_unit.lower() in elm_s.lower():   #minutes
+				minutes = str(elm.find("span", { "class" : "text-2"}).text).lstrip()
+				prev = "mins"
+
+
+			if sms_unit.lower() in elm_s.lower():   #minutes
+				sms = str(elm.find("span", { "class" : "text-2"}).text).lstrip()
+				prev = "sms"
+
+
+			if renew_keyword.lower() in elm_s.lower() or once_keyword.lower() in elm_s.lower():
+				if prev == "mins": minutes_type = renew_keyword
+				if prev == "sms": sms_type = renew_keyword
+
+				prev = "type"
+
+
+parse_info(t)
 
 print("Credit: " + credit + " " + credit_unit)
 print("Credit expiration: " + credit_expire)
--- kaktus_client.py
+++ kaktus_client.py
@@ -8,140 +8,168 @@ import re
 
 import html
 
-def unescape(str_i):
-	#print(type(str_i))
-	#return html.parser.HTMLParser().unescape(str_i)#.encode(), 'utf-8')
-	#soup = BeautifulSoup(str_i, "html.parser")
-	#return unquote(str_i.decode())
-	#print(soup)
-	#return soup.pretiffy()
-	return html.unescape(str_i.decode())
+class kaktus:
+	def __init__(self):
+		self.session = requests.Session()
 
 
-def cookies_from_dict(dict):
-	jar = requests.cookies.RequestsCookieJar()
-
-	for key, value in dict.items():
-		jar.set(key, value)
-
-	return jar
+	def set_proper_headers(self):
+		# 'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+		self.session.headers['Accept-Language'] = 'cs,sk;q=0.8,en-US;q=0.5,en;q=0.3'
+		self.session.headers['User-Agent'] = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0'
 
 
 
-def set_proper_cookies(session):
-	cookies = session.cookies.get_dict()
+	def receive_first_cookies(self):
+		r = self.session.get('https://www.mujkaktus.cz/moje-sluzby', allow_redirects=False)
+		#r.encoding = 'utf-8'
+		'''
+		cookies = self.session.cookies.get_dict()
+		cookies['GUEST_LANGUAGE_ID'] = 'cs_CZ'
+		cookies['AJAXIBLE_JAVASCRIPT_ENABLED'] = 'true'
+		'''
+		print(r.status_code)
+		#print(r.history)
+		#print(r.reason)
+		#print(cookies)
 
-	cookies['GUEST_LANGUAGE_ID'] = 'cs_CZ'
-	cookies['AJAXIBLE_JAVASCRIPT_ENABLED'] = 'true'
+		if r.status_code != 200:
+			raise Exception("Status code %d during fething first cookies" % r.status_code)
 
-	session.cookies = cookies_from_dict(cookies)
-
-	return session
-
-
-
-def set_proper_headers(session):
-	# 'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-	session.headers['Accept-Language'] = 'cs,sk;q=0.8,en-US;q=0.5,en;q=0.3'
-	session.headers['User-Agent'] = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0'
-
-	return session
-
+		self.r = r
 
 
-def set_XHR_header(session):
-	session.headers['X-Requested-With'] = 'XMLHttpRequest'
-	return session
+	def login(self, username, password):
+		'''
+		return values:
+		 0 - all is OK
+
+		'''
+
+		self.set_proper_headers()
+		self.receive_first_cookies()
+
+		data = {}
+		data['username'] = username
+		data['password'] = password
+		data['submit'] = 'Vstoupit'
+
+		#headers = { 'Referer' : 'https://www.mujkaktus.cz/moje-sluzby', 'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Language' : 'cs,sk;q=0.8,en-US;q=0.5,en;q=0.3', 'User-Agent' : 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0' }
+
+		print("Log in - POST")
+		r = self.session.post('https://www.mujkaktus.cz/.gang/login-url', allow_redirects=False, data = data)
+		#r.encoding = 'utf-8'
+
+		
+		#print(cookies)
+
+		print(r.status_code)
+		print(r.history)
+		print(r.reason)
+
+		if r.status_code != 302:
+			if "chyby: 701" in r.text:
+				raise Exception("Login failed, probably because of 3 incorrect logins")
+
+			else:
+				#print(r.text)
+				raise Exception("Login failed, unknown reason")
+
+		self.r = r
+
+
+	def unescape(self, str_i):
+		#print(type(str_i))
+		#return html.parser.HTMLParser().unescape(str_i)#.encode(), 'utf-8')
+		#soup = BeautifulSoup(str_i, "html.parser")
+		#return unquote(str_i.decode())
+		#print(soup)
+		#return soup.pretiffy()
+		return html.unescape(str_i.decode())
+
+
+	def cookies_from_dict(self, dict):
+		jar = requests.cookies.RequestsCookieJar()
+
+		for key, value in dict.items():
+			jar.set(key, value)
+
+		return jar
 
 
 
-def receive_first_cookies(session):
-	r = session.get('https://www.mujkaktus.cz/moje-sluzby', allow_redirects=False)
-	#r.encoding = 'utf-8'
-	'''
-	cookies = session.cookies.get_dict()
-	cookies['GUEST_LANGUAGE_ID'] = 'cs_CZ'
-	cookies['AJAXIBLE_JAVASCRIPT_ENABLED'] = 'true'
-	'''
-	print(r.status_code)
-	#print(r.history)
-	#print(r.reason)
-	#print(cookies)
+	def set_proper_cookies(self):
+		cookies = self.session.cookies.get_dict()
 
-	return session
+		cookies['GUEST_LANGUAGE_ID'] = 'cs_CZ'
+		cookies['AJAXIBLE_JAVASCRIPT_ENABLED'] = 'true'
 
-
-
-def login(session, username, password):
-	'''
-	return values:
-	 0 - all is OK
-
-	'''
-	data = {}
-	data['username'] = username
-	data['password'] = password
-	data['submit'] = 'Vstoupit'
-
-	#headers = { 'Referer' : 'https://www.mujkaktus.cz/moje-sluzby', 'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Language' : 'cs,sk;q=0.8,en-US;q=0.5,en;q=0.3', 'User-Agent' : 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0' }
-
-	print("Log in - POST")
-	r = session.post('https://www.mujkaktus.cz/.gang/login', allow_redirects=False, data = data)
-	#r.encoding = 'utf-8'
+		self.session.cookies = self.cookies_from_dict(cookies)
 
 	
-	#print(cookies)
-
-	print(r.status_code)
-	print(r.history)
-	print(r.reason)
-
-	return session
-
-def get_info_url(session):
-	print("Getting URL for info - GET")
-	r = session.get('https://www.mujkaktus.cz/moje-sluzby', allow_redirects=False)
-
-	'''cookies = session.cookies.get_dict()
-	cookies['GUEST_LANGUAGE_ID'] = 'cs_CZ'
-	print(cookies)
-	'''
-	print(r.status_code)
-	print(r.history)
-	print(r.reason)
-	#print(unescape(r.content))
-	soup = BeautifulSoup(r.content, 'html.parser')
-	url = soup.find("div", { "data-invocation-url" : re.compile(r"https://.*") })['data-invocation-url']
-	#print(url)
-	return session, url
 
 
-def logout(session):
-	r = session.get('https://www.mujkaktus.cz/.gang/logout')
-	print(r.status_code)
-
-	return session
+	def set_XHR_header(self):
+		self.session.headers['X-Requested-With'] = 'XMLHttpRequest'
 
 
 
-username = ""
+	def get_info_url(self):
+		print("Getting URL for info - GET")
+		r = self.session.get('https://www.mujkaktus.cz/moje-sluzby', allow_redirects=False)
+
+		'''cookies = self.session.cookies.get_dict()
+		cookies['GUEST_LANGUAGE_ID'] = 'cs_CZ'
+		print(cookies)
+		'''
+		print(r.status_code)
+		print(r.history)
+		print(r.reason)
+		print(self.unescape(r.content))
+
+		soup = BeautifulSoup(r.content, 'html.parser')
+		url = soup.find("div", { "data-invocation-url" : re.compile(r"https://.*") })['data-invocation-url']
+		#print(url)
+		return url
+
+
+	def get_info_html(self):
+		self.set_proper_cookies()
+
+		url = self.get_info_url()
+
+		self.set_XHR_header()
+
+		r = self.session.get(url, allow_redirects=False)
+
+		print(r.status_code)
+		print(r.reason)
+		return self.unescape(r.content)
+
+
+	def logout(self):
+		r = self.session.get('https://www.mujkaktus.cz/.gang/logout')
+		print(r.status_code)
+
+		self.r = r
+
+
+
+
+cli = kaktus()
+
+import sys
+username = sys.argv[1]
 password = ""
 
 
 print("Your kaktus password:")
 password = getpass.getpass()
 
-session = requests.Session()
 
 
+cli.login(username, password)
 
-
-
-session = set_proper_headers(session)
-session = receive_first_cookies(session)
-session = login(session, username, password)
-
-session = set_proper_cookies(session)
 #print(unescape(r.content))
 
 
@@ -149,22 +177,20 @@ session = set_proper_cookies(session)
 
 
 
-session, f = get_info_url(session)
-session = set_XHR_header(session)
+#self.session, f = get_info_url(self)
+
 #params = { 'p_p_id' : 'kaktusvcc_WAR_vcc', 'p_p_lifecycle' : 2, 'p_p_state' : 'normal', 'p_p_mode' : 'view', 'p_p_cacheability' : 'cacheLevelPage', 'p_p_col_id' : 'column-1', 'p_p_col_pos' : 1, 'p_p_col_count' : 2, '_kaktusvcc_WAR_vcc_userAction' : 'dashboard.enter', '_kaktusvcc_WAR_vcc_from' : 'dashboard', '_kaktusvcc_WAR_vcc_vccDaemonUrl' : 1 }
 
-r = session.get(f, allow_redirects=False)
-print(r.status_code)
-print(r.reason)
-print(unescape(r.content))
 
 
-session, f = get_info_url(session)
-r = session.get(f, allow_redirects=False)
-print(r.status_code)
-print(r.reason)
-print(unescape(r.content))
+import datetime
+html = cli.get_info_html()
 
-logout(session)
+with open("data_%s_log.txt" % str(datetime.datetime.now()).replace(" ", "-"), "w") as f:
+	f.write(html)
 
-print(session.headers)
+print(html)
+
+cli.logout()
+
+print(cli.session.headers)
